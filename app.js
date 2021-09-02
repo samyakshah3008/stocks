7828f0949d4eebd29fdd8d154f348dc0f54d4ec0
Samyak Shah <samyakshah3008@gmail.com>
2021-09-02 13:56:15 +0530

    FEAT: change bg on profit and loss FIX : ignored negative values

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ var currentPrice = document.querySelector('#current-price')
 var submitbtn = document.querySelector('#submit-btn')
 var outputBox = document.querySelector('#output-box')
 
-submitbtn.addEventListener('click',submitHandler)
+
 
 function submitHandler(){
     var ip = Number(initialPrice.value)
     var qty = Number(stocksQuantity.value)
     var curr = Number(currentPrice.value)
-
+if (ip>=0 && qty> 0 && curr>= 0) {
     calculateProfitAndLoss(ip,qty,curr)
+} 
+    else {
+      alert("Please enter valid value")
+      document.body.classList.remove("lossBg")
+      document.body.classList.remove("profitBg")
+    }
 }
 
 function calculateProfitAndLoss(initial,quantity,current){
@@ -20,18 +26,25 @@ function calculateProfitAndLoss(initial,quantity,current){
       var loss = (initial - current) * quantity
       var lossPercentage = (loss/initial) * 100
       showOutput('Loss is ' + loss + ' and the loss percentage is ' + lossPercentage + '%')
-      outputBox.style.color = "red"
+      outputBox.style.color = "white"
       outputBox.style.fontSize = "xx-large"
+       document.body.classList.remove("profitBg")
+      document.body.classList.add("lossBg")
     }else if (current > initial){
         var profit = (current - initial) * quantity
       var profitPercentage = (profit/initial) * 100
      showOutput('The profit is ' + profit + ' and profit percentage is ' + profitPercentage + '%')
-     outputBox.style.color = "green"
+     outputBox.style.color = "white"
      outputBox.style.fontSize = "xx-large"
+     document.body.classList.add("profitBg")
+     document.body.classList.remove("lossBg")
+
     }else {
      showOutput('No pain No gain')
      outputBox.style.color = "#D97706"
      outputBox.style.fontSize = "xx-large"
+    
+
     }
 
 
@@ -40,3 +53,5 @@ function calculateProfitAndLoss(initial,quantity,current){
 function showOutput(message) {
    outputBox.innerHTML = message
 }
+
+submitbtn.addEventListener("click",submitHandler)
